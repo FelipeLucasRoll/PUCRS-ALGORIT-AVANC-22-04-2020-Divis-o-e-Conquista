de44f6d151efffbecfc88022f0e62152ccb847cb
FelipeLucasRoll <57493030+FelipeLucasRoll@users.noreply.github.com>
2020-05-17 15:25:04 -0300

    contagem de intrucoes

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,56 @@
 
 public class Main {
 
+	
+
 	public static final int[] size     = {32, 2048, 1_048_576};
 	public static final int[] sizeLong = {4 , 16  , 64};
 	
-	public static int[] vetor1 = RandomUtil.randomVector(size[0]); 
+	public static int[] vetor1 = RandomUtil.randomVector(size[1]); 
 	public static int[] vetor2 = RandomUtil.randomVector(size[0]); 
 	
 	public static void main(String[] args) {
 		MainService service = new MainService();
-		service.mergeSort(vetor1, size[0]);
+		service.mergeSort(vetor1, size[1]);
 		int maxVal1 = service.maxVal1(vetor2, vetor2.length);
 		int maxVal2 = service.maxVal2(vetor2, 0, vetor2.length-1);
 		long multiply = service.multiply(RandomUtil.randomLong(), RandomUtil.randomLong(), sizeLong[2]);
 		
+		long tempoInicio;
+		long tempoFinal; 
+
+		System.out.println("Merge Sort" + "\n");
+		tempoInicio = System.currentTimeMillis();
 		System.out.println("TAMANHO: " + size[0]);
-		System.out.println("RESULT MergeSort: " + printVector(vetor1));
-		System.out.println("RESULT MaxVal1  : " + maxVal1);
-		System.out.println("RESULT MaxVal2  : " + maxVal2);
-		System.out.println("RESULT multiply : " + multiply);
+		System.out.println("RESULT: " + printVector(vetor1));
+		tempoFinal = System.currentTimeMillis();
+		System.out.printf("Tempo de execucao: " + "%.3f ms%n", (tempoFinal - tempoInicio) / 1000d);
+		System.out.println("Numero de iteracoes MergeSort :" + service.getIteracoesMergeSort());
+		System.out.println("---------------------------------------------------------------------------------------------------------------------");
+
+		System.out.println("MaxVal1" + "\n");
+		tempoInicio = System.currentTimeMillis();
+		System.out.println("RESULT : " + maxVal1);
+		tempoFinal = System.currentTimeMillis();
+		System.out.printf("Tempo de execucao: " + "%.3f ms%n", (tempoFinal - tempoInicio) / 1000d);
+		System.out.println("Numero de iteracoes maxVal1 :" + service.getIteracoesMaxVal1());
+		System.out.println("---------------------------------------------------------------------------------------------------------------------");
+
+		System.out.println("MaxVal2" + "\n");
+		tempoInicio = System.currentTimeMillis();
+		System.out.println("RESULT : " + maxVal2);
+		tempoFinal = System.currentTimeMillis();
+		System.out.printf("Tempo de execucao: " + "%.3f ms%n", (tempoFinal - tempoInicio) / 1000d);
+		System.out.println("Numero de iteracoes maxVal2 :" + service.getIteracoesMaxVal2());
+		System.out.println("---------------------------------------------------------------------------------------------------------------------");
+
+
+		System.out.println("Multiply" + "\n");
+		tempoInicio = System.currentTimeMillis();
+		System.out.println("RESULT: " + multiply);
+		tempoFinal = System.currentTimeMillis();
+		System.out.printf("Tempo de execucao: " + "%.3f ms%n", (tempoFinal - tempoInicio) / 1000d);
+		System.out.println("Numero de iteracoes multiply :" + service.getIteracoesMultiply());
 		
 	}
 	
--- src/MainService.java
+++ src/MainService.java
@@ -1,78 +1,181 @@
 
 public class MainService {
 
+	public static int iteracoesMultiply;
+	public static int iteracoesMaxVal1;
+	public static int iteracoesMaxVal2;
+	public static int iteracoesMergeSort;
+
+	
+	public int getIteracoesMaxVal1(){
+		return iteracoesMaxVal1;
+	}
+
+
+	public void adicionaIteracaoMaxVal1(int i){
+		iteracoesMaxVal1 = iteracoesMaxVal1 + i;
+		
+	}
+
+	
+	public int getIteracoesMaxVal2(){
+		return iteracoesMaxVal2;
+	}
+
+
+	public void adicionaIteracaoMaxVal2(int i){
+		iteracoesMaxVal2 = iteracoesMaxVal2 + i;
+		
+	}
+
+	public int getIteracoesMergeSort(){
+		return iteracoesMergeSort;
+	}
+
+
+	public void adicionaIteracaoMergeSort(int i){
+		iteracoesMergeSort = iteracoesMergeSort + i;
+		
+	}
+
+
+
+	public int getIteracoesMultiply(){
+		return iteracoesMultiply;
+	}
+
+	public void adicionaIteracaoMultiply(int i){
+		iteracoesMultiply = iteracoesMultiply + i;
+		
+	}
+
+
 	public long multiply(long x, long y, long n) { 
-	   if(n == 1)
-	      return x * y;
-	   else {
+
+
+	   if(n == 1){
+		adicionaIteracaoMultiply(3);
+	     return  x * y;
+	}else {
+		
 		  long m, a, b, c, d, e, f, g, h;
+		 adicionaIteracaoMultiply(9);
 		  
-	      m = n/2;
-	      a = (long)(x/Math.pow(2, m));
-	      b = (long)(x%Math.pow(2, m));
-	      c = (long)(Math.abs(y/Math.pow(2, m))); 
-	      d = (long)(y%Math.pow(2, m));
-	      e = multiply(a, c, m);
-	      f = multiply(b, d, m);
-	      g = multiply(b, c, m);
-	      h = multiply(a, d, m);
-	      return (long)(Math.pow(2,2*m)*e + Math.pow(2, m*(g + h)) + f);
+		  m = n/2;
+		  adicionaIteracaoMultiply(2);
+
+		  a = (long)(x/Math.pow(2, m));
+		  adicionaIteracaoMultiply(3);
+
+		  b = (long)(x%Math.pow(2, m));
+		  adicionaIteracaoMultiply(3);
+
+		  c = (long)(Math.abs(y/Math.pow(2, m))); 
+		  adicionaIteracaoMultiply(3);
+
+		  d = (long)(y%Math.pow(2, m));
+		  adicionaIteracaoMultiply(3);
+
+		  e = multiply(a, c, m);
+		  adicionaIteracaoMultiply(2);
+		  
+		  f = multiply(b, d, m);
+		  adicionaIteracaoMultiply(2);
+
+		  g = multiply(b, c, m);
+		  adicionaIteracaoMultiply(2);
+
+		  h = multiply(a, d, m);
+		  adicionaIteracaoMultiply(11);
+		 
+		 
+		  return  (long)(Math.pow(2,2*m)*e + Math.pow(2, m*(g + h)) + f);
 	   }
 	}
 	
 	public int maxVal1(int A[], int n) {  
+
 		int max = A[0];
+		adicionaIteracaoMaxVal1(2);
 		for (int i = 1; i < n; i++) {  
+			adicionaIteracaoMaxVal1(3);
 			if( A[i] > max ) 
 			   max = A[i];
+			   adicionaIteracaoMaxVal1(4);
+	
 		}
+		adicionaIteracaoMaxVal1(1);
+		
 		return max;
 	}
 
 	public int maxVal2(int A[], int init, int end) {
-		if (end - init <= 1)
-			return Math.max(A[init], A[end]);
-		else {
+
+		if (end - init <= 1){
+			adicionaIteracaoMaxVal2(3);
+			return Math.max(A[init], A[end]);	
+		}else {
 			int m = (init + end) / 2;
 			int v1 = maxVal2(A, init, m);
 			int v2 = maxVal2(A, m + 1, end);
-			return Math.max(v1, v2);
+			adicionaIteracaoMaxVal2(7);
+			
+			return  Math.max(v1, v2);
 		}
+			
+	
 	}
 
 	public void mergeSort(int[] vetor, int size) {
-		if (size < 2)
+	
+		if (size < 2){
+			adicionaIteracaoMergeSort(3);
 			return;
-
+		}
 		int metade = size / 2;
 		int[] vetorA = new int[metade];
 		int[] vetorB = new int[size - metade];
+		adicionaIteracaoMergeSort(8);
 
-		for (int i = 0; i < metade; i++)
+		for (int i = 0; i < metade; i++){
 			vetorA[i] = vetor[i];
+			adicionaIteracaoMergeSort(i + 3);
+		}
 
-		for (int i = metade; i < size; i++)
+		for (int i = metade; i < size; i++){
 			vetorB[i - metade] = vetor[i];
-
+			adicionaIteracaoMergeSort(i+4);
+		}
 		mergeSort(vetorA, metade);
 		mergeSort(vetorB, size - metade);
 
 		merge(vetor, vetorA, vetorB, metade, size - metade);
+		adicionaIteracaoMergeSort(5);
+		
 	}
 
 	public void merge(int[] vetor, int[] vetorA, int[] vetorB, int sizeA, int sizeB) {
 		int i = 0, j = 0, k = 0;
+		adicionaIteracaoMergeSort(3);
 
 		while (i < sizeA && j < sizeB) {
-			if (vetorA[i] <= vetorB[j])
+			adicionaIteracaoMergeSort(i);
+			if (vetorA[i] <= vetorB[j]){
 				vetor[k++] = vetorA[i++];
-			else
+				adicionaIteracaoMergeSort(6);
+			}else{
 				vetor[k++] = vetorB[j++];
+				adicionaIteracaoMergeSort(3);
+			}
 		}
 
-		while (i < sizeA)
+		while (i < sizeA){
 			vetor[k++] = vetorA[i++];
-		while (j < sizeB)
+			adicionaIteracaoMergeSort(i + 3);
+		}
+		while (j < sizeB){
 			vetor[k++] = vetorB[j++];
+			adicionaIteracaoMergeSort(j + 3);
+		}
 	}
 }
